e4d34fb2fedd9796328b2d81dae9e6d3d1e1d77c
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2012-06-15 17:23:58 -0400

    added paper

--- examplets/lqr_uncontrollable.py
+++ examplets/lqr_uncontrollable.py
@@ -13,19 +13,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 A = np.matrix([[2,0],
-              [0,2]])
+              [0,3]])
 
-B = np.matrix([1,-1]).T
+B = np.matrix([1,.1]).T
 
 
 Q = np.matrix([[1,0],
-               [0,1]])
+               [0,-1]])
 R = np.eye(1)
 
 T = 20
 gain_schedule,cost_to_go_schedule = dtfh_lqr(A,B,Q,R,T-1)
 
-x0 = np.array([1,1])
+x0 = np.array([2,1])
 
 xsol, usol = simulate_lti_fb_dt(A,B,x0,-gain_schedule,T)
 
